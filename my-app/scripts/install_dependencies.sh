10d06a46396fab81bc075e413499ad15cf55578d
Himadri <122869644+Himadri2@users.noreply.github.com>
2024-04-21 22:24:20 +0530

    Update install_dependencies.sh

--- my-app/scripts/install_dependencies.sh
+++ my-app/scripts/install_dependencies.sh
@@ -20,20 +20,20 @@ execute_command() {
 cd /var/www/html/laravel/ || { echo "Error: Unable to navigate to Laravel directory." >&2; exit 1; }
 
 # Update composer dependencies
-execute_command composer update
+execute_command /opt/remi/php81/root/usr/bin/php composer update
 
 # Install composer dependencies
-execute_command composer install
+execute_command /opt/remi/php81/root/usr/bin/php composer install
 
 # Check if APP_KEY is blank in .env file
 if grep -q "^APP_KEY=$" .env; then
     # Generate key if it's blank
-    execute_command php artisan key:generate
+    execute_command /opt/remi/php81/root/usr/bin/php artisan key:generate
 else
     echo "APP_KEY is already set in .env file." >> "$LOG_FILE"
 fi
 
 # Run migrations
-execute_command php artisan migrate
+execute_command /opt/remi/php81/root/usr/bin/php artisan migrate
 
 echo "All commands executed successfully."
