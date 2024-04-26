5dc8d8f36a649f2f21e0430d4adc34d7e383ccb6
root <root@ip-10-0-0-163.ap-south-1.compute.internal>
2024-04-26 12:16:33 +0000

    added

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = "users";
-
     /**
      * The attributes that are mass assignable.
      *
@@ -42,6 +40,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
