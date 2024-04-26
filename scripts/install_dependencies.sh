c88dec9749dd143e1464962329954324cdcdfc89
root <root@ip-10-0-0-163.ap-south-1.compute.internal>
2024-04-26 12:40:47 +0000

    added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,22 +23,24 @@ execute_command() {
 cd /var/www/html/laravel/ || { echo "Error: Unable to navigate to Laravel directory." >&2; exit 1; }
 
 # Update composer dependencies
-execute_command /usr/local/bin/composer update
+# /usr/local/bin/
+execute_command composer update
 
 # Install composer dependencies
-execute_command /usr/local/bin/composer install
+execute_command composer install
 
 # Check if APP_KEY is blank in .env file
+# /opt/remi/php81/root/usr/bin/
 if grep -q "^APP_KEY=$" .env; then
     # Generate key if it's blank
-    execute_command /opt/remi/php81/root/usr/bin/php artisan key:generate
+    execute_command php artisan key:generate
 else
     echo "APP_KEY is already set in .env file." >> "$LOG_FILE"
 fi
 
 # Run migrations
-execute_command /opt/remi/php81/root/usr/bin/php artisan migrate:fresh
-execute_command /opt/remi/php81/root/usr/bin/php artisan migrate
+execute_command php artisan migrate:fresh
+execute_command php artisan migrate
 
 # Change ownership and permissions
 execute_command chown -R apache:apache /var/www/html/*
